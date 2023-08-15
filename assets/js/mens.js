f197eb56a9ef6e3a6bb4bb3d0f979abbb036a5f8
StrongereCommerce <86730095+StrongereCommerce@users.noreply.github.com>
2023-08-15 13:30:05 -0400

    Update mens.js

--- assets/js/mens.js
+++ assets/js/mens.js
@@ -181,7 +181,7 @@ document
 let sizeData = {};
 
 // Fetch the JSON data
-fetch('./assets/js/converted_data.json')
+fetch('./assets/js/mens.json')
     .then(response => response.json())
     .then(data => {
         sizeData = data;
